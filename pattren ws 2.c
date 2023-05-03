ef9133496955d06a2298215c8051898dabf91b7c
srushtivjm <127848798+srushtivjm@users.noreply.github.com>
2023-05-03 12:26:15 +0530

    Add files via upload

---
+++ pattren ws 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int r,c;
+	for(r=1;r<=5;r++)
+     {
+       for(c=1;c<=r;c++)
+       {
+       		printf("%d",c);
+	   }
+	   printf("\n");
+	 }
+	 for(r=4;r>=1;r--)
+     {
+       for(c=1;c<=r;c++)
+       {
+       		printf("%d",c);
+	   }
+	   printf("\n");
+	 }
+}
